c345cb3bd2d9afbc8f0d23d53a8cabcc6cb6b5b5
Nazar Stepaniuk <41264703+vicksod@users.noreply.github.com>
2018-07-19 13:11:25 +0300

    Add files via upload

--- MyCompany/JS/script.js
+++ MyCompany/JS/script.js
@@ -7,3 +7,15 @@ menu.onclick = function myFunction() {
 		x.className = "topnav";
 	}
 }
+var $back__to__top = $(".back__to__top")
+$(window).on("scroll", function(){
+	if ($(window).scrollTop() >= 20) {
+		$back__to__top.fadeIn();
+	}
+	else{
+		$back__to__top.fadeOut();
+	}
+});
+$back__to__top.on("click", function() {
+	$("html,body").animate({scrollTop:0}, 900)
+});
